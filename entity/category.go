6a3d104665df9d01fccd6c83a0c7572a20000d6f
Wandana <dn.wandana@gmail.com>
2021-07-23 11:36:24 +0700

    add entity

---
+++ entity/category.go
@@ -0,0 +1,12 @@
+package entity
+
+import "time"
+
+type Category struct {
+	ID           int64
+	CategoryName string
+	CategorySlug string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    time.Time
+}

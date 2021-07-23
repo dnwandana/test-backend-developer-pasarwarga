c6b198e402c4040f4737110cb2036d1a6873d65d
Wandana <dn.wandana@gmail.com>
2021-07-23 16:20:34 +0700

    implement controller

--- controller/article_controller.go
+++ controller/article_controller.go
@@ -1,54 +1,105 @@
 package controller
 
 import (
+	"github.com/dnwandana/test-backend-developer-pasarwarga/model"
 	"github.com/dnwandana/test-backend-developer-pasarwarga/service"
+	"github.com/dnwandana/test-backend-developer-pasarwarga/util"
 	"github.com/gofiber/fiber/v2"
 )
 
 type ArticleController struct {
-	articleService *service.ArticleService
+	ArticleService service.ArticleService
 }
 
 func NewArticleController(articleService *service.ArticleService) ArticleController {
 	return ArticleController{
-		articleService: articleService,
+		ArticleService: *articleService,
 	}
 }
 
 func (controller *ArticleController) SetupRoutes(app *fiber.App) {
+	app.Get("/article/deleted", controller.ListSoftDeleted)
+	app.Delete("/article/deleted/:id", controller.Delete)
 	app.Post("/article", controller.Create)
 	app.Get("/article", controller.List)
 	app.Get("/article/:id", controller.FindOne)
 	app.Put("/article/:id", controller.Update)
 	app.Delete("/article/:id", controller.SoftDelete)
-	app.Get("/article/deleted", controller.ListSoftDeleted)
-	app.Delete("/article/deleted/:id", controller.Delete)
 }
 
 func (controller *ArticleController) Create(ctx *fiber.Ctx) error {
-	return ctx.SendString("Create new article")
+	var request *model.ArticleCreateRequest
+	parserErr := ctx.BodyParser(&request)
+	util.ReturnErrorIfNeeded(parserErr)
+
+	controller.ArticleService.Create(request)
+
+	return ctx.Status(fiber.StatusCreated).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusCreated,
+		Data:       "Article created",
+	})
 }
 
 func (controller *ArticleController) List(ctx *fiber.Ctx) error {
-	return ctx.SendString("List all article")
+	articles := controller.ArticleService.List()
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       articles,
+	})
 }
 
 func (controller *ArticleController) FindOne(ctx *fiber.Ctx) error {
-	return ctx.SendString("Find specific article")
+	articleID := ctx.Params("id")
+
+	article := controller.ArticleService.FindOne(articleID)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       article,
+	})
 }
 
 func (controller *ArticleController) Update(ctx *fiber.Ctx) error {
-	return ctx.SendString("Update article")
+	articleID := ctx.Params("id")
+
+	var request *model.ArticleUpdateRequest
+	parserErr := ctx.BodyParser(&request)
+	util.ReturnErrorIfNeeded(parserErr)
+
+	controller.ArticleService.Update(articleID, request)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       "Article updated",
+	})
 }
 
 func (controller *ArticleController) SoftDelete(ctx *fiber.Ctx) error {
-	return ctx.SendString("Soft delete article")
+	articleID := ctx.Params("id")
+
+	controller.ArticleService.SoftDelete(articleID)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       "Article deleted",
+	})
 }
 
 func (controller *ArticleController) ListSoftDeleted(ctx *fiber.Ctx) error {
-	return ctx.SendString("List all soft deleted article")
+	articles := controller.ArticleService.ListSoftDeleted()
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       articles,
+	})
 }
 
 func (controller *ArticleController) Delete(ctx *fiber.Ctx) error {
-	return ctx.SendString("Delete article")
+	articleID := ctx.Params("id")
+
+	controller.ArticleService.Delete(articleID)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       "Article deleted from database",
+	})
 }
--- controller/category_controller.go
+++ controller/category_controller.go
@@ -1,54 +1,105 @@
 package controller
 
 import (
+	"github.com/dnwandana/test-backend-developer-pasarwarga/model"
 	"github.com/dnwandana/test-backend-developer-pasarwarga/service"
+	"github.com/dnwandana/test-backend-developer-pasarwarga/util"
 	"github.com/gofiber/fiber/v2"
 )
 
 type CategoryController struct {
-	categoryService *service.CategoryService
+	CategoryService service.CategoryService
 }
 
 func NewCategoryController(categoryService *service.CategoryService) CategoryController {
 	return CategoryController{
-		categoryService: categoryService,
+		CategoryService: *categoryService,
 	}
 }
 
 func (controller *CategoryController) SetupRoutes(app *fiber.App) {
+	app.Get("/category/deleted", controller.ListSoftDeleted)
+	app.Delete("/category/deleted/:id", controller.Delete)
 	app.Post("/category", controller.Create)
 	app.Get("/category", controller.List)
 	app.Get("/category/:id", controller.FindOne)
 	app.Put("/category/:id", controller.Update)
 	app.Delete("/category/:id", controller.SoftDelete)
-	app.Get("/category/deleted", controller.ListSoftDeleted)
-	app.Delete("/category/deleted/:id", controller.Delete)
 }
 
 func (controller *CategoryController) Create(ctx *fiber.Ctx) error {
-	return ctx.SendString("Create new category")
+	var request *model.CategoryCreateRequest
+	parserErr := ctx.BodyParser(&request)
+	util.ReturnErrorIfNeeded(parserErr)
+
+	controller.CategoryService.Create(request)
+
+	return ctx.Status(fiber.StatusCreated).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusCreated,
+		Data:       "Category created",
+	})
 }
 
 func (controller *CategoryController) List(ctx *fiber.Ctx) error {
-	return ctx.SendString("List all category")
+	categories := controller.CategoryService.List()
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       categories,
+	})
 }
 
 func (controller *CategoryController) FindOne(ctx *fiber.Ctx) error {
-	return ctx.SendString("Find specific category")
+	categoryID := ctx.Params("id")
+
+	category := controller.CategoryService.FindOne(categoryID)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       category,
+	})
 }
 
 func (controller *CategoryController) Update(ctx *fiber.Ctx) error {
-	return ctx.SendString("Update specific category")
+	categoryID := ctx.Params("id")
+
+	var request *model.CategoryUpdateRequest
+	parserErr := ctx.BodyParser(&request)
+	util.ReturnErrorIfNeeded(parserErr)
+
+	controller.CategoryService.Update(categoryID, request)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       "Category updated",
+	})
 }
 
 func (controller *CategoryController) SoftDelete(ctx *fiber.Ctx) error {
-	return ctx.SendString("Soft delete category")
+	categoryID := ctx.Params("id")
+
+	controller.CategoryService.SoftDelete(categoryID)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       "Category deleted",
+	})
 }
 
 func (controller *CategoryController) ListSoftDeleted(ctx *fiber.Ctx) error {
-	return ctx.SendString("List all soft deleted category")
+	categories := controller.CategoryService.ListSoftDeleted()
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       categories,
+	})
 }
 
 func (controller *CategoryController) Delete(ctx *fiber.Ctx) error {
-	return ctx.SendString("Delete category")
+	categoryID := ctx.Params("id")
+
+	controller.CategoryService.Delete(categoryID)
+
+	return ctx.Status(fiber.StatusOK).JSON(model.SuccessResponse{
+		StatusCode: fiber.StatusOK,
+		Data:       "Category deleted from database",
+	})
 }
